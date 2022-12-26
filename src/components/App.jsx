72359368dd0ae3f1a2cc1a162d360de22cdf5643
Evhenii Babii <113521371+Yevhenii-Babii@users.noreply.github.com>
2022-12-26 18:33:46 +0200

    add task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,61 @@
 import React from 'react';
 import { List } from './List/List';
-import Picture from '../images/picture.png'
-import Line from '../images/Line.png'
-import Portray from '../images/Boy.png'
+import Picture from '../images/picture.png';
+import Line from '../images/Line.png';
+import Portray from '../images/Boy.png';
 import css from '../styles/app.module.css';
-import Logo from '../images/Logo.png'
-
+import Logo from '../images/Logo.png';
 
 export class App extends React.Component {
-  state={
-    followers: 100500
-   }
+  state = {
+    followers: Number(localStorage.getItem('followers')) || 100500,
+  };
 
-   onButtonClick =() => {
-    if (this.state.followers===100500) { this.setState({ 
-      followers: this.state.followers + 1
-    })
-  } else {
-    this.setState({ 
-      followers: this.state.followers - 1
-  })}
-  }
+  onButtonClick = () => {
+    if (this.state.followers === 100500) {
+      this.setState({
+        followers: this.state.followers + 1,
+      });
+      localStorage.setItem('followers', this.state.followers + 1);
+    } else {
+      this.setState({
+        followers: this.state.followers - 1,
+      });
 
-  addItem() {
-    let items = JSON.parse(localStorage.getItem('BtnActive'))
-    items.push()
-    localStorage.setItem('BtnActive', JSON.stringify(items))
-  }
-  
+      localStorage.setItem('followers', this.state.followers - 1);
+    }
+  };
+
   render() {
-  return (
-    <section className={css.wrapper}>
-    <div className={css.card}>
-      <img src={Logo} alt="Logo" className={css.logo} />
-      <img src={Picture} alt="Background" className={css.picture}/>
-      <img src={Line} alt="" className={css.line} />
-      <img src={Portray} alt="" className={css.portray}/>
-<List followers={this.state.followers}/>
-{this.state.followers===100500 ? <button type="button" className={css.button} onClick={this.onButtonClick}> Follow </button> : <button type="button" className={css.btn__active} onClick={this.onButtonClick}> Following </button> }
-</div>  
-</section>  
-  );
-
+    return (
+      <section className={css.wrapper}>
+        <div className={css.card}>
+          <img src={Logo} alt="Logo" className={css.logo} />
+          <img src={Picture} alt="Background" className={css.picture} />
+          <img src={Line} alt="" className={css.line} />
+          <img src={Portray} alt="" className={css.portray} />
+          <List followers={this.state.followers} />
+          {this.state.followers === 100500 ? (
+            <button
+              type="button"
+              className={css.button}
+              onClick={this.onButtonClick}
+            >
+              {' '}
+              Follow{' '}
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={css.btn__active}
+              onClick={this.onButtonClick}
+            >
+              {' '}
+              Following{' '}
+            </button>
+          )}
+        </div>
+      </section>
+    );
+  }
 }
-};
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,19 @@
-import css from '../List/List.module.css'
 
-export const List = ({followers}) => {
-return (
+import PropTypes from "prop-types";
+import css from '../List/List.module.css';
+
+export const List = ({ followers }) => {
+  return (
     <ul className={css.card__list}>
-<li className={css.item__tweet}>777 Tweets</li>
-<li className={css.item__followers}> {followers.toLocaleString("en")} FOLLOWERS</li>
+      <li className={css.item__tweet}>777 Tweets</li>
+      <li className={css.item__followers}>
+        {' '}
+        {followers.toLocaleString('en')} FOLLOWERS
+      </li>
     </ul>
-)
+  );
+};
 
-}
+List.propTypes ={ 
+followers: PropTypes.number.isRequired
+};
